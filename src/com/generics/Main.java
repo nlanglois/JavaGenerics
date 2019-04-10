5cc4f0b2ee7e21bec2260755fd9a70482ee2ac41
Nikolai Langlois <nikolai.langlois@bsci.com>
2019-04-09 21:42:45 -0500

    Example of ClassCastException error due to type casting

--- src/com/generics/Main.java
+++ src/com/generics/Main.java
@@ -1,8 +1,21 @@
 package com.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        List list = new ArrayList();
+
+        list.add("abc");
+        list.add(new Integer(5));
+
+        for (Object obj: list) {
+            // type casting leading to ClassCastException at runtime
+            String str = (String) obj;
+        }
+
     }
 }
